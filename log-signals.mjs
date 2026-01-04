a40215ba427c2a5e1828be4cb9573743d11e4b5f
Paul Murray <plmrry@gmail.com>
2026-01-03 23:05:00 -0500

    feat: add signal logging functionality with all, exit, and non-exit signals

---
+++ log-signals.mjs
@@ -0,0 +1,53 @@
+export const allSignals = new Set([
+  "SIGABRT",
+  "SIGALRM",
+  "SIGBUS",
+  "SIGCHLD",
+  "SIGCLD",
+  "SIGCONT",
+  "SIGEMT",
+  "SIGFPE",
+  "SIGHUP",
+  "SIGILL",
+  "SIGINFO",
+  "SIGINT", // Listening for this prevents exit on Control-C
+  "SIGIO",
+  "SIGIOT",
+  "SIGKILL", // Listening for this prevents exit on Control-C
+  "SIGLOST",
+  "SIGPIPE",
+  "SIGQUIT",
+  "SIGSEGV",
+  "SIGSTOP", // Listening for this prevents exit on Control-C
+  "SIGTSTP",
+  "SIGSYS",
+  "SIGTERM",
+  "SIGTRAP",
+  "SIGTTIN",
+  "SIGTTOU",
+  "SIGUNUSED",
+  "SIGURG",
+  "SIGUSR1",
+  "SIGUSR2",
+  "SIGVTALRM",
+]);
+
+/**
+ * Listening for these will throw an error
+ */
+export const exitSignals = new Set(["SIGINT", "SIGKILL", "SIGSTOP"]);
+
+export const nonExitSignals = allSignals.difference(exitSignals);
+
+export function logSignals(process, type = "non-exit") {
+  const signals = {
+    all: allSignals,
+    exit: exitSignals,
+    "non-exit": nonExitSignals,
+  }[type];
+  for (const signal of signals) {
+    process.on(signal, () => {
+      console.log(`wut.mjs got signal: ${signal}`);
+    });
+  }
+}
